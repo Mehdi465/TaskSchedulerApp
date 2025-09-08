0606e34504b0377a562a3a48ace166c948a27084
Mehdi465 <mehdi.fellahi06@gmail.com>
2025-09-09 00:00:58 +0200

    added standard dimens

---
+++ app/src/main/java/com/example/taskscheduler/ui/theme/Dimens.kt
@@ -0,0 +1,12 @@
+package com.example.taskscheduler.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val spaceXXS = 2.dp
+    val spaceXS = 4.dp
+    val spaceS = 8.dp
+    val spaceM = 16.dp
+    val spaceL = 24.dp
+    val spaceXL = 32.dp
+}
